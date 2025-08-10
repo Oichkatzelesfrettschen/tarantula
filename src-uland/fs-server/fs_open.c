6c01bab0b03e6d12ef329dd392fa1cd25f5c21d7
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-08-09 18:17:14 -0700

    fs-server: remove redundant sys/types include

--- src-uland/fs-server/fs_open.c
+++ src-uland/fs-server/fs_open.c
@@ -1,12 +1,7 @@
 /* Simple wrapper used by the exokernel file hook. */
-#include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include "fs_server.h"
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int
-fs_open(const char *path, int flags)
-{
-    return open(path, flags, 0);
-}
+int fs_open(const char *path, int flags) { return open(path, flags, 0); }
