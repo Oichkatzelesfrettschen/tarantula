0015b820f134355239682685378a0db8e4a9ec16
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-08-21 18:40:28 -0700

    Use generic compilers for tests

--- tests/sched_stub.c
+++ tests/sched_stub.c
@@ -5,8 +5,8 @@ SPINLOCK_DEFINE(sched_lock);
 void sched_lock_acquire(void) {}
 void sched_lock_release(void) {}
 
-_BitInt(32) runin = 0;
-_BitInt(32) runout = 0;
+run_counter_t runin = 0;
+run_counter_t runout = 0;
 void sched_increment_runin(void) { runin++; }
 void sched_increment_runout(void) { runout++; }
 
